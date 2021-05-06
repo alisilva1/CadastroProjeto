998c942ed5bdda42acbd6e171985498090770560
MarcosHayman <57717714+MarcosFRHayman@users.noreply.github.com>
2021-05-06 19:18:11 -0300

    Adiciona versão inicial do GET
    adiciona dependencia do Thymeleaf
    Adiciona GetMapping em /projeto para o Thymeleaf (Thymeleaf não implementado ainda)
    Adiciona GetMapping em /projetoTeste para retornar todos elementos do banco de dados, utilizado para testar

--- Projeto_B/src/main/java/com/teamb/project/controller/ProjectController.java
+++ Projeto_B/src/main/java/com/teamb/project/controller/ProjectController.java
@@ -1,2 +1,33 @@
-package com.teamb.project.controller;public class ProjectController {
+package com.teamb.project.controller;
+
+import com.teamb.project.dao.ProjectDAO;
+import com.teamb.project.model.Project;
+import com.teamb.project.service.ProjectImpService;
+import com.teamb.project.service.ProjectService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.servlet.ModelAndView;
+
+import java.util.List;
+
+@RestController
+public class ProjectController {
+    @Autowired
+    private ProjectImpService projectImpService;
+
+    //Função para testar: Retorna todos os itens
+    @GetMapping("/projetoTeste")
+    public List<Project> listAll(){
+        return projectImpService.findAll();
+    }
+
+    //Função para quando Thymeleaf estiver implementado
+    @GetMapping("/projeto")
+    public ModelAndView listar(){
+        ModelAndView modelAndView = new ModelAndView("ListaProjeto");
+        modelAndView.addObject("titulo", projectImpService.findAll());
+        modelAndView.addObject(new Project());
+        return modelAndView;
+    }
 }
