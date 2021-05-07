3a68c2a926627b74f36af2e4101479d6516ae4c6
MarcosHayman <57717714+MarcosFRHayman@users.noreply.github.com>
2021-05-07 15:36:18 -0300

    implementa GET para um id específico
    Quando não encontrado ele retorna um valor String no ResponseEntity, não sei se essa
    é a melhor forma de se fazer.

--- Projeto_B/src/main/java/com/teamb/project/controller/ProjectController.java
+++ Projeto_B/src/main/java/com/teamb/project/controller/ProjectController.java
@@ -1,15 +1,17 @@
 package com.teamb.project.controller;
 
-import com.teamb.project.dao.ProjectDAO;
 import com.teamb.project.model.Project;
 import com.teamb.project.service.ProjectImpService;
-import com.teamb.project.service.ProjectService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.servlet.ModelAndView;
 
 import java.util.List;
+import java.util.NoSuchElementException;
 
 @RestController
 public class ProjectController {
@@ -18,16 +20,27 @@ public class ProjectController {
 
     //Função para testar: Retorna todos os itens
     @GetMapping("/projetoTeste")
-    public List<Project> listAll(){
+    public List<Project> listAll() {
         return projectImpService.findAll();
     }
 
     //Função para quando Thymeleaf estiver implementado
     @GetMapping("/projeto")
-    public ModelAndView listar(){
+    public ModelAndView Listar() {
         ModelAndView modelAndView = new ModelAndView("ListaProjeto");
         modelAndView.addObject("titulo", projectImpService.findAll());
         modelAndView.addObject(new Project());
         return modelAndView;
+    }
+
+    @GetMapping(path = "/{id}")
+    public ResponseEntity<?> getProjectByid(@PathVariable("id") Integer id) {
+        Project project;
+        try {
+            project = projectImpService.findById(id);
+        } catch (NoSuchElementException e) {
+            return new ResponseEntity<>("Elemento não encontrado", HttpStatus.NOT_FOUND);//Não sei se há problema
+        }                                                                                 //em retornar um valor String
+        return new ResponseEntity<>(project, HttpStatus.OK);
     }
 }
